5001785211477d0d8f01dbff383abd2d41398ba5
nguyenngochan1506 <nguyenngochandeveloper@gmail.com>
2023-12-29 22:51:49 +0700

    cập nhật lab-07

--- js/group-diary.js
+++ js/group-diary.js
@@ -20,7 +20,7 @@ const week = (n) => {
             <td>Nguyễn Ngọc Hân</td>
             <td>26-12-2023</td>
             <td>29-12-2023</td>
-            <td></td>
+            <td>hoàn thành</td>
         </tr>
         <tr>
             <th>#2</th>
@@ -28,7 +28,7 @@ const week = (n) => {
             <td>Lê Bá Khánh Duy</td>
             <td>26-12-2023</td>
             <td>29-12-2023</td>
-            <td></td>
+            <td>hoàn thành</td>
         </tr>
         <tr>
             <th>#3</th>
@@ -36,7 +36,7 @@ const week = (n) => {
             <td>Hoàng Lê Nguyên Mạnh</td>
             <td>26-12-2023</td>
             <td>29-12-2023</td>
-            <td></td>
+            <td>hoàn thành</td>
         </tr>
         <tr>
             <th>#4</th>
@@ -44,7 +44,7 @@ const week = (n) => {
             <td>Đăng Văn Trung</td>
             <td>26-12-2023</td>
             <td>29-12-2023</td>
-            <td></td>
+            <td>chưa hoàn thành</td>
         </tr>
         <tr>
             <th>#5</th>
@@ -52,8 +52,111 @@ const week = (n) => {
             <td>Nguyễn Vũ Bảo</td>
             <td>26-12-2023</td>
             <td>29-12-2023</td>
-            <td></td>
-        </tr>`
+            <td>hoàn thành</td>
+        </tr>
+        `
+       document.querySelector('.my-file').innerHTML=`
+       <div class="h3">Các File Thiết Kế</div>
+                <ul class="d-flex">
+                    <li><strong>Hân</strong>
+                        <ul>
+                            <li>Home Page: 
+                                <ul>
+                                    <li>Giấy: <a class="link-info" href="./img/lab-07/homepage.png" target="_blank" rel="noopener noreferrer">File Giấy</a></li>
+                                    <li>Figma: <a class="link-info" href="https://www.figma.com/community/file/1322581491249749941" target="_blank">Link Figma</a></li>
+                                </ul>
+                            </li>
+                            <li>Group-Diary: 
+                                <ul>
+                                    <li>Giấy: <a class="link-info" href="./img/lab-07/group-diary.png" target="_blank" rel="noopener noreferrer">File Giấy</a></li>
+                                    <li>Figma: <a class="link-info" href="https://www.figma.com/community/file/1322583971132871609" target="_blank">Link Figma</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <br>
+                    <li><strong>Trung</strong>
+                        <ul>
+                            <li>Details-Page: 
+                                <ul>
+                                    <li>Giấy: <strong>null</strong></li>
+                                    <li>Figma: <strong>null</strong></li>
+                                </ul>
+                            </li>
+                            <li>Category-Page: 
+                                <ul>
+                                    <li>Giấy: <strong>null</strong></li>
+                                    <li>Figma: <strong>null</strong></li>
+                                </ul>
+                            </li>
+                            <li>Anime-Favorite-Page: 
+                                <ul>
+                                    <li>Giấy: <strong>null</strong></li>
+                                    <li>Figma: <strong>null</strong></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <br>
+                    <li><strong>Bảo</strong>
+                        <ul>
+                            <li>Login: 
+                                <ul>
+                                    <li>Giấy: <a class="link-info" href="./img/lab-07/login.jpg" target="_blank" rel="noopener noreferrer">File Giấy</a></li>
+                                    <li>Figma: <a class="link-info" href="https://www.figma.com/file/X4o8RNTHll3IrrIuKXH3Up/Login?type=design&mode=design&t=g83DUzwyHMmawGti-0" target="_blank">Link Figma</a></li>
+                                </ul>
+                            </li>
+                            <li>Admin-User-Page: 
+                                <ul>
+                                    <li>Giấy: <a class="link-info" href="./img/lab-07/admin-user-page.jpg" target="_blank" rel="noopener noreferrer">File Giấy</a></li>
+                                    <li>Figma: <a class="link-info" href="https://www.figma.com/file/IB02lMI3S4QaxvXJfg7RLT/page-user?type=design&node-id=0-1&mode=design&t=KYBjZe39Fb5XcrOG-0" target="_blank">Link Figma</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <br>
+                    <li><strong>Mạnh</strong>
+                        <ul>
+                            <li>Watching-Page: 
+                                <ul>
+                                    <li>Giấy: <a class="link-info" href="./img/lab-07/watchingpage.jpg" target="_blank" rel="noopener noreferrer">File Giấy</a></li>
+                                    <li>Figma: <a class="link-info" href="https://www.figma.com/file/hDuepywanvNOqjxevRDZaO/Untitled?type=design&node-id=0%3A1&mode=design&t=SM2DxZ7VQW4MtBZt-1" target="_blank">Link Figma</a></li>
+                                </ul>
+                            </li>
+                            <li>Admin-Home-Page: 
+                                <ul>
+                                    <li>Giấy: <a class="link-info" href="./img/lab-07/admin-home-page.jpg" target="_blank" rel="noopener noreferrer">File Giấy</a></li>
+                                    <li>Figma: <a class="link-info" href="https://www.figma.com/file/hDuepywanvNOqjxevRDZaO/Untitled?type=design&node-id=0%3A1&mode=design&t=SM2DxZ7VQW4MtBZt-1" target="_blank">Link Figma</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <br>
+                    <li><strong>Duy</strong>
+                        <ul>
+                            <li>Sigup: 
+                                <ul>
+                                    <li>Giấy: <a class="link-info" href="./img/lab-07/sigup.jpg" target="_blank" rel="noopener noreferrer">File Giấy</a></li>
+                                    <li>Figma: <a class="link-info" href="https://www.figma.com/file/dziDi2j4dU2HXh2sQ4N6VA/Design-for-Animeweb?type=design&node-id=0%3A1&mode=design&t=PE7BOOTWFKejUJGj-1" target="_blank">Link Figma</a></li>
+                                </ul>
+                            </li>
+                            <li>About: 
+                                <ul>
+                                    <li>Giấy: <a class="link-info" href="./img/lab-07/about.jpg" target="_blank" rel="noopener noreferrer">File Giấy</a></li>
+                                    <li>Figma: <a class="link-info" href="https://www.figma.com/file/dziDi2j4dU2HXh2sQ4N6VA/Design-for-Animeweb?type=design&node-id=0%3A1&mode=design&t=PE7BOOTWFKejUJGj-1" target="_blank">Link Figma</a></li>
+                                </ul>
+                            </li>
+                            <li>Admin-Anime-Page: 
+                                <ul>
+                                    <li>Giấy: <a class="link-info" href="./img/lab-07/admin-anime-page.jpg" target="_blank" rel="noopener noreferrer">File Giấy</a></li>
+                                    <li>Figma: <a class="link-info" href="https://www.figma.com/file/dziDi2j4dU2HXh2sQ4N6VA/Design-for-Animeweb?type=design&node-id=0%3A1&mode=design&t=PE7BOOTWFKejUJGj-1" target="_blank">Link Figma</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+       `
+
             document.querySelector('#week-7').classList.add('my-active')
             table.innerHTML = customHtml;
             weekTitle.innerHTML = "(Week-7)"
