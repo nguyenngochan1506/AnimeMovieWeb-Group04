9c70111f078e9e8fa2b11cf03682f5311dc6aeaa
nguyenngochan1506 <nguyenngochandeveloper@gmail.com>
2024-02-01 12:46:48 +0700

    hoàn thành CRUD (Hân)

--- js/anime-detail.js
+++ js/anime-detail.js
@@ -1,20 +1,113 @@
 import { HOST_NAME, getParamUrl } from "./utils.js";
-window.onload  = ()=>{
+window.onload = () => {
     //lay id anime tu url 
     const animeId = getParamUrl('animeId')
-  
+    const token = localStorage.getItem('token')
+    if(token){
+        document.querySelector('#my-form-comment').addEventListener('submit', (e)=> handleSubmitComment(e, animeId))
+    }else{
+
+        document.querySelector('#my-form-comment').innerHTML = '<span>Vui lòng đăng nhập để bình luận <a class="btn btn-danger" href="./login.html">Đăng Nhập</a> </span>';
+    }
+
     fetch(`${HOST_NAME}/anime/${animeId}`)
-    .then(respnse => respnse.json())
-    .then(data =>{
-        document.querySelector('.anime__details__title h1').innerHTML = data.name;
-        document.querySelector('.anime__details__title span').innerHTML = `${data.englishName}/ ${data.vietnamName}`
-        document.querySelector('.anime__details__text p').innerHTML = data.description;
-        document.querySelector('.anime__image-box img').setAttribute('src', data.coverImage);
-        document.querySelector('.view').innerHTML = data.view.toLocaleString();
-        document.querySelector('.releaseYear').innerHTML = data.releaseYear;
-        document.querySelector('#btn-xem-ngay').addEventListener('click', ()=>{
-            window.location.href = `./watching.html?animeId=${data.id}&tap=1`
+        .then(respnse => respnse.json())
+        .then(data => {
+            document.querySelector('.anime__details__title h1').innerHTML = data.name;
+            document.querySelector('.anime__details__title span').innerHTML = `${data.englishName}/ ${data.vietnamName}`
+            document.querySelector('.anime__details__text p').innerHTML = data.description;
+            document.querySelector('.anime__image-box img').setAttribute('src', data.coverImage);
+            document.querySelector('.view').innerHTML = data.view.toLocaleString();
+            document.querySelector('.releaseYear').innerHTML = data.releaseYear;
+            document.querySelector('#btn-xem-ngay').addEventListener('click', () => {
+                window.location.href = `./watching.html?animeId=${data.id}&tap=1`
+            })
+        })
+
+    getAllComment(animeId).then(data => {
+        let customHtml = '';
+        data.forEach(comment =>{
+            customHtml += `
+                <div class="anime__review__item d-flex align-items-start mb-3">
+                <div class="anime__review__item__pic">
+                <img height="50px" width="50px"  src="${comment.user.avatar ||'./img/usericon.jpg'}" alt="" />
+                </div>
+                <div class="anime__review__item__text">
+                <h6 class="fw-bold">
+                    ${comment.user.fullName}
+                </h6>
+                <p>
+                    ${comment.description}
+                </p>
+                </div>
+            </div>
+            `;
+            document.querySelector('.anime__details__review').innerHTML = customHtml;
         })
     })
-    //phan chuyen trang
+
+    
 }
+
+const handleSubmitComment = async (e, animeId) =>{
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const comment = formData.get('comment');
+    if(!comment){
+        alert("Vui lòng nhập đầy đủ thông tin!");
+        return;
+    }
+    try {
+        const userId = JSON.parse(localStorage.getItem('user')).id
+        const request = {
+            description: comment,
+            user: `user/${userId}`,
+            anime:`anime/${animeId}`
+        }
+        const response = await fetch(`${HOST_NAME}/binh-luan`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json"
+            },
+            body:JSON.stringify(request)
+        })
+        const data = await response.json();
+        alert("Bạn đã comment")
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+const getAllComment = async (animeId) => {
+    try {
+        const response = await fetch(`${HOST_NAME}/binh-luan/search/findAllByAnime_Id?id=${animeId}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "Application/json"
+            }
+        })
+        const data = await response.json();
+        const listComment = data._embedded.comments;
+
+        const listPromise =  listComment.map(async (comment) => {
+            return  {
+                description: comment.description,
+                user: await loadUserByUrl(comment._links.user.href)
+            }
+        })
+        const result = Promise.all(listPromise)
+
+        return result
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const loadUserByUrl = async (url) => {
+
+    const urlObject = new URL(url);
+    const path = urlObject.pathname;
+    const response = await fetch(`${HOST_NAME}${path}`);
+    const data = await response.json();
+    return data;
+};
--- js/utils.js
+++ js/utils.js
@@ -1,28 +1,73 @@
 const HOST_NAME = "https://anime-web-production-996b.up.railway.app";
-function parseJwt (token) {
+function parseJwt(token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
 
     return JSON.parse(jsonPayload);
 }
 
-const getParamUrl = (param)=>{
+const getParamUrl = (param) => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const result = urlParams.get(param)
     return result;
 }
 
-const debounce = (callBack, delay) =>{
+const debounce = (callBack, delay) => {
     let timeout;
-    return (...args)=>{
+    return (...args) => {
         clearTimeout(timeout);
         timeout = setTimeout(() => {
             callBack(...args);
         }, delay);
     }
 }
-export {HOST_NAME, parseJwt, getParamUrl, debounce};
+const getBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function () {
+        resolve(reader.result);
+      };
+      reader.onerror = function (error) {
+        reject(error);
+      };
+    });
+  };
+
+const getAllCategories = async () => {
+    try {
+        const response = await fetch(`${HOST_NAME}/the-loai`, {
+            method:"GET",
+            headers:{
+                "Content-Type": "Application/json"
+            }
+        })
+        const data = await response.json();
+        return data._embedded.categories;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getCategoriesOfAnime = async (animeId)=>{
+    try {
+        const response = await fetch(`${HOST_NAME}/anime/${animeId}/categoryList`, {
+            method:"GET",
+            headers:{
+                "Content-Type": "Application/json"
+            }
+        })
+        const data = await response.json();
+        const categories = data._embedded.categories;
+        const categoriesId = categories.map(cate=>cate.id);
+        return categoriesId;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export { HOST_NAME, parseJwt, getParamUrl, debounce ,getAllCategories, getCategoriesOfAnime, getBase64};
